12388130dc822d9f473a93d5738aaea091129dfc
rasmusp <porthinr@arcada.fi>
2024-11-18 14:04:15 +0200

    add predetermined mass to ship

--- Assets/scripts/ufoCcontrolerRg.cs
+++ Assets/scripts/ufoCcontrolerRg.cs
@@ -2,9 +2,10 @@ using UnityEngine;
 
 public class UfoController : MonoBehaviour
 {
-    // Set the speed of the UFO
-    public float speed = 4f;
-    public float rotationSpeed = 150f;
+   
+    public float speed = 4f; // Set the speed of the UFO
+    public float rotationSpeed = 150f; // set  rotation
+    public float mass = 20; // Predetermined mass
 
     // Reference to the Rigidbody2D component
     private Rigidbody2D rb;
@@ -12,13 +13,14 @@ public class UfoController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
         // Get the Rigidbody2D component attached to the UFO
         rb = GetComponent<Rigidbody2D>();
-      
+
+        // Set the mass of the Rigidbody2D
+        rb.mass = mass;
 
         // Set the UFO's starting position to (0, 0) + zindex
-        transform.position = new Vector3(0.0f, 0.0f,-2f);
+        transform.position = new Vector3(0.0f, 0.0f, -2f);
     }
 
     // Update is called once per frame
@@ -28,8 +30,9 @@ public class UfoController : MonoBehaviour
         float rotationInput = Input.GetAxis("Horizontal");
         float thrustInput = Input.GetAxis("Vertical");
 
-         rb.rotation -= rotationInput * rotationSpeed * Time.deltaTime;
-        
+        // Rotate the UFO
+        rb.rotation -= rotationInput * rotationSpeed * Time.deltaTime;
+
         // Apply force to the Rigidbody2D component for movement
         Vector2 thrust = transform.up * thrustInput * speed;
         rb.AddForce(thrust);
